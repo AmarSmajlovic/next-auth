cb6ce3e6434767715ca2e17e66c3ebeab31cc1dd
AmarSmajlovic <amarcsgo@gmail.com>
2023-06-25 17:54:57 +0200

    refactored code and added types

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import App, { AppContext, AppProps } from "next/app";
+import { AppContext, AppProps } from "next/app";
 import { UserDetailsProvider } from "@/contexts";
 import { getSession } from "@/auth";
 
@@ -17,25 +17,9 @@ export default function MyApp({ Component, session, ...props }: MyAppProps) {
 
 MyApp.getInitialProps = async (appCtx: AppContext) => {
   const ctx = appCtx.ctx;
-  const publicPages = ["/login"];
   const session = await getSession(ctx);
 
-  if (session || (!session && publicPages.includes(ctx.pathname))) {
-    const props = await App.getInitialProps(appCtx);
-    return {
-      ...props,
-      session,
-    };
-  }
-
-  if (ctx.res) {
-    ctx.res.writeHead(307, {
-      Location: `http://localhost:3001/login`,
-    });
-    return ctx.res.end();
-  } else {
-    // @ts-ignore
-    window.location = `http://localhost:3001/login`;
-    await new Promise((resolve) => {});
-  }
+  return {
+    session,
+  };
 };
--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -1,9 +1,11 @@
+import { decodeRefreshToken } from "@/utils";
 import axios from "axios";
 
 class UserService {
-  async getUserDetails(id: string) {
+  async getUserDetails() {
     try {
-      const res = await axios.get(`http://localhost:3000/users/${id}`);
+      const token = decodeRefreshToken(); // in order to get sub (id of user)
+      const res = await axios.get(`http://localhost:3000/users/${token.sub}`);
 
       return res.data;
     } catch (error: any) {
